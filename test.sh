cf1e3c271736262063618d806085887dfe9f04ff
vishalvilas nikam <vishalvilasv4n@gmail.com>
2019-11-17 19:07:59 +0530

    added

--- test.sh
+++ test.sh
@@ -4,8 +4,8 @@
   docker container stop my_db_doc_container
   docker container rm my_db_doc_container
  fi
- if  [ $(docker image ls -q --filter name=my_app_db)!='' ];then
+ if  [ $(docker image ls -q --filter reference=my_db_doc)!='' ];then
  docker image rm my_db_doc
  fi
  docker build -t my_app_doc .
-docker run  -itd -p 9099:3306 --name my_db_doc _container my_db_doc
+ docker run  -itd -p 9099:3306 --name my_db_doc _container my_db_doc
